674c55de12cc096563d781c337ad950857867778
sebahauzer124 <sebahauzer124@gmail.com>
2022-04-01 16:28:51 -0300

    se sube

---
+++ src/app/mock-task.ts
@@ -0,0 +1,29 @@
+import { Task } from "./task"
+
+
+export const TASK=[
+{id:1,
+     text:"Termina primer modulo de Angular",
+     day:"Agosto 5 a las 1200",
+     reninder:true
+},
+
+{id:2,
+    text:"Hacer las compras para la cena",
+    day:"Agosto 5 a las 1500",
+    reninder:true
+    },
+
+{id:3,
+        text:"Leer bootstrap",
+        day:"Agosto 5 a las 1700",
+        reninder:false
+        },
+
+{id:4,
+   text:"Mi libro favorito",
+   day:"Agosto 5 a las 2100",
+   reninder:false
+            },
+
+]
